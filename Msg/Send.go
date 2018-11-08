bfe8413015756a0e53e49bc3d6e10933a9a225da
wang <641257395@qq.com>
2018-11-08 22:34:43 +0800

    fix

--- Msg/Send.go
+++ Msg/Send.go
@@ -20,26 +20,18 @@ func SendMessage(conn net.Conn, data []byte) {
 	fmt.Println("data send leng:", n, err)
 }
 
-//func SendSuccessFailMessage(conn net.Conn, msg string) {
-//	p1 := &protobuf.BackContent{}
-//	p1.Id = _const.RCV_SUCCESS
-//	p1.Msg = msg
-//	data, _ := proto.Marshal(p1)
-//	conn.Write(data)
-//}
-
 func SendFailMessage(conn net.Conn, msg string) {
-	sendMessage(conn, msg, _const.RCV_FAIL)
+	sendSuccessFailMessage(conn, msg, _const.RCV_FAIL)
 }
 
 func SendSuccessMessage(conn net.Conn, msg string) {
-	sendMessage(conn, msg, _const.RCV_SUCCESS)
+	sendSuccessFailMessage(conn, msg, _const.RCV_SUCCESS)
 }
 
-func sendMessage(conn net.Conn, msg string, msg_id int32) {
+func sendSuccessFailMessage(conn net.Conn, msg string, msg_id int32) {
 	p1 := &protobuf.BackContent{}
 	p1.Id = msg_id
 	p1.Msg = msg
 	data, _ := proto.Marshal(p1)
-	conn.Write(data)
+	SendMessage(conn, data)
 }
